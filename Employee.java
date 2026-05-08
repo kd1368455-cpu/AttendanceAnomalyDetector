dd1c31039b450257dde5638258bdc6199aababc9
kd1368455-cpu <kd1368455@st.kobedenshi.ac.jp>
2026-05-08 11:47:32 +0900

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Employee {
+	private String employeeId;
+    private String name;
+    public Employee(String employeeId, String name) {
+        this.employeeId = employeeId;
+        this.name = name;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
